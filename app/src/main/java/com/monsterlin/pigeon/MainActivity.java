8b58c1d44ea22a842b3458bdc26939c36380bb30
monsterlin <monster941025@gmail.com>
2017-07-09 16:15:33 +0800

    add fragment

--- app/src/main/java/com/monsterlin/pigeon/MainActivity.java
+++ app/src/main/java/com/monsterlin/pigeon/MainActivity.java
@@ -1,9 +1,13 @@
 package com.monsterlin.pigeon;
 
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
 import android.view.View;
 
 import com.monsterlin.pigeon.base.BaseActivity;
+import com.monsterlin.pigeon.view.HomeFragment;
+import com.monsterlin.pigeon.view.PersonFragment;
+import com.monsterlin.pigeon.view.ToolsFragment;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
@@ -17,7 +21,9 @@ import com.roughike.bottombar.OnTabSelectListener;
  */
 public class MainActivity extends BaseActivity {
 
+    private int curIndex = -1;
     private BottomBar mBottomBar;
+    private String[] tags = {"homeFragment", "toolsFragment", "personFragment"};
 
     @Override
     public int getLayoutId() {
@@ -36,12 +42,16 @@ public class MainActivity extends BaseActivity {
             public void onTabSelected(@IdRes int tabId) {
                 switch (tabId) {
                     case R.id.tab_home:
+                        setSelect(0);
                         break;
                     case R.id.tab_chat:
+                        //跳转到Activity
                         break;
                     case R.id.tab_tools:
+                        setSelect(1);
                         break;
                     case R.id.tab_person:
+                        setSelect(2);
                         break;
                 }
             }
@@ -50,7 +60,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initData() {
-
     }
 
     @Override
@@ -59,4 +68,35 @@ public class MainActivity extends BaseActivity {
     }
 
 
+    private void setSelect(int i) {
+        if (curIndex == i)
+            return;
+
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(tags[i]);
+        if (fragment == null) {
+            switch (i) {
+                case 0:
+                    fragment = new HomeFragment();
+                    break;
+                case 1:
+                    fragment = new ToolsFragment();
+                    break;
+                case 2:
+                    fragment = new PersonFragment();
+                    break;
+                default:
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().add(R.id.main_contentContainer, fragment, tags[i]).commit();
+        }
+
+        for (int j = 0; j < 3; j++) {
+            Fragment f = getSupportFragmentManager().findFragmentByTag(tags[j]);
+            if (f != null) {
+                getSupportFragmentManager().beginTransaction().hide(f).commit();
+            }
+        }
+        getSupportFragmentManager().beginTransaction().show(fragment).commit();
+
+    }
 }
---
+++ app/src/main/java/com/monsterlin/pigeon/view/PersonFragment.java
@@ -0,0 +1,42 @@
+package com.monsterlin.pigeon.view;
+
+import android.view.View;
+
+import com.monsterlin.pigeon.R;
+import com.monsterlin.pigeon.base.BaseFragment;
+
+/**
+ * @author : monsterLin
+ * @version : 1.0
+ * @email : monster941025@gmail.com
+ * @github : https://github.com/monsterLin
+ * @time : 2017/7/9
+ * @desc : 个人中心
+ */
+
+public class PersonFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_person;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
