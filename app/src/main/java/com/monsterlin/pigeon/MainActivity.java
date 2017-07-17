2913dedd3181ae568d58df50ebcd807d1bcfa7a0
monsterlin <monster941025@gmail.com>
2017-07-17 11:48:46 +0800

    :fire: improve branch measurement

--- app/src/main/java/com/monsterlin/pigeon/MainActivity.java
+++ app/src/main/java/com/monsterlin/pigeon/MainActivity.java
@@ -1,18 +1,8 @@
 package com.monsterlin.pigeon;
 
-import android.support.annotation.IdRes;
-import android.support.v4.app.Fragment;
-import android.view.View;
-
-import com.monsterlin.pigeon.base.BaseActivity;
-import com.monsterlin.pigeon.utils.ToastUtils;
-import com.monsterlin.pigeon.view.HomeFragment;
-import com.monsterlin.pigeon.view.PersonFragment;
-import com.monsterlin.pigeon.view.ToolsFragment;
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
-
-import cn.bmob.v3.BmobUser;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  *
@@ -46,85 +36,11 @@ import cn.bmob.v3.BmobUser;
  * @desc : MainActivity
  * @version: 1.0
  */
-public class MainActivity extends BaseActivity {
-
-    private int curIndex = -1;
-    private BottomBar mBottomBar;
-    private String[] tags = {"homeFragment", "toolsFragment", "personFragment"};
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public int getLayoutId() {
-        return R.layout.activity_main;
-    }
-
-    @Override
-    public void initViews() {
-        mBottomBar = findView(R.id.main_bottom_bar);
-    }
-
-    @Override
-    public void initListener() {
-        mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelected(@IdRes int tabId) {
-                switch (tabId) {
-                    case R.id.tab_home:
-                        setSelect(0);
-                        break;
-                    case R.id.tab_chat:
-                        //跳转到Activity
-                        break;
-                    case R.id.tab_tools:
-                        setSelect(1);
-                        break;
-                    case R.id.tab_person:
-                        setSelect(2);
-                        break;
-                }
-            }
-        });
-    }
-
-    @Override
-    public void initData() {
-        ToastUtils.showToast(this, BmobUser.getCurrentUser().getObjectId());
-    }
-
-    @Override
-    public void processClick(View v) {
-
-    }
-
-
-    private void setSelect(int i) {
-        if (curIndex == i)
-            return;
-
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag(tags[i]);
-        if (fragment == null) {
-            switch (i) {
-                case 0:
-                    fragment = new HomeFragment();
-                    break;
-                case 1:
-                    fragment = new ToolsFragment();
-                    break;
-                case 2:
-                    fragment = new PersonFragment();
-                    break;
-                default:
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().add(R.id.main_contentContainer, fragment, tags[i]).commit();
-        }
-
-        for (int j = 0; j < 3; j++) {
-            Fragment f = getSupportFragmentManager().findFragmentByTag(tags[j]);
-            if (f != null) {
-                getSupportFragmentManager().beginTransaction().hide(f).commit();
-            }
-        }
-        getSupportFragmentManager().beginTransaction().show(fragment).commit();
-
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
