37a36522d95cff6a1b50f076b30e04bc178b42ac
monsterlin <monster941025@gmail.com>
2017-07-18 23:25:59 +0800

    add TuLing config

---
+++ app/src/main/java/com/monsterlin/pigeon/constant/TuLingConfig.java
@@ -0,0 +1,18 @@
+package com.monsterlin.pigeon.constant;
+
+/**
+ * @author : monsterLin
+ * @version : 1.0
+ * @email : monster941025@gmail.com
+ * @github : https://github.com/monsterLin
+ * @time : 2017/7/18
+ * @desc : 图灵机器人配置信息
+ * @api ： http://www.tuling123.com/openapi/api
+ */
+public class TuLingConfig {
+
+    /*
+     * TuLing APIKey
+     */
+    public static final String APIKEY = "fcd7f1a8fd0b4048ae880e545d1ccf7b";
+}
